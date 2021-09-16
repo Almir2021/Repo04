3b46d56d07aac9ada26edaff6f6c2cd1c2d2158f
Almir2021 <bosnjicalmir@gmail.com>
2021-09-16 15:26:39 +0200

    Dodajem vjezbu sa ostatkom (%)

---
+++ ex07/src/index.js
@@ -0,0 +1,14 @@
+function main(numOne, numTwo) {
+    //Only change code below this line 
+    var numOne;
+    var numTwo;
+    var remainder;
+
+    remainder = numOne % numTwo;
+
+    //Only change code above this line
+    return remainder;
+}
+console.log(main(15, 4));
+
+module.exports = main;
