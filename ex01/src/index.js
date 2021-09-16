6833aa6949547d663626f6588e1b78fefec3c5db
Almir2021 <bosnjicalmir@gmail.com>
2021-09-16 12:47:13 +0200

    Dodajem vjezbu 1 djeljenje

---
+++ ex01/src/index.js
@@ -0,0 +1,24 @@
+function main(numOne, numTwo) {
+
+    var quotient;
+    if (numTwo === 0) {
+        quotient = "Divide by 0! Cannot divide by zero!";
+
+
+    } else {
+
+
+
+        //Only change code below this line 
+        var numOne;
+        var numTwo;
+        quotient = numOne / numTwo;
+
+
+        //Only change code below this line 
+    }
+    return quotient;
+}
+console.log(main(55, 0));
+
+module.exports = main;
