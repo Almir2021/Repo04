2d30b5d4c0322b76a992f29558e7cd304561f3df
Almir2021 <bosnjicalmir@gmail.com>
2021-09-16 17:32:11 +0200

    dodajem vjezbu 10

---
+++ ex10/src/index.js
@@ -0,0 +1,17 @@
+function main() {
+    //Only change code below this line 
+    var myfirstName = "Almir";
+    var myLastName = "Bosnjic";
+    var myHometown = "Tuzla"
+
+    //Only change code above this line
+    return {
+        myfirstName,
+        myLastName,
+        myHometown
+    };
+}
+
+console.log(main());
+
+module.exports = main;
