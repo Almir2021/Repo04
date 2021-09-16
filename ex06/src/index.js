19b2c0602037699414b2fc578a24e338c221f51a
Almir2021 <bosnjicalmir@gmail.com>
2021-09-16 15:05:56 +0200

    Dodajem vjezbu djeljenja dvije decimalne vrijednosti

---
+++ ex06/src/index.js
@@ -0,0 +1,14 @@
+function main(numOne, numTwo) {
+    //Only change code below this line 
+    var numOne;
+    var numTwo;
+    var myQuotient;
+
+    myQuotient = numOne / numTwo;
+
+    //Only change code above this line
+    return myQuotient;
+}
+console.log(main(16.5, 5.5));
+
+module.exports = main;
